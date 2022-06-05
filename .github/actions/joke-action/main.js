3e35cac167015b3ba491b988977ccd0b435c0744
Jaysok <wooljs@gmail.com>
2022-06-06 07:44:06 +0900

    fix joke-action

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -1,8 +1,8 @@
-const joke = require('./joke');
+const getJoke = require('./joke');
 const core = require('@actions/core');
 
 async function run() {
-  const joke = await joke();
+  const joke = await getJoke();
   console.log(joke);
   core.setOutput("joke-output", joke);
 }
